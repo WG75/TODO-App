103de6d263e12e6a4961b699e94f20f75fdde064
Doubleu Gee <Waleed.gamal1000@gmail.com>
2017-04-04 22:24:11 +0200

    done with the markup and most of the styling

--- app/script/es5/script.js
+++ app/script/es5/script.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('hiiiiii');
+Vue.component('hi', {
+  props: ['todo'],
+  template: '<li>{{ todo }}</li>'
+});
+
+var list = new Vue({
+  el: '#list',
+  data: {
+    items: [{ item: 'item1' }, { item: 'item2' }, { item: 'item3' }]
+  }
+});
--- app/script/es6/script.js
+++ app/script/es6/script.js
@@ -1,1 +1,12 @@
-  console.log('hiiiiii')
+Vue.component('hi', {
+  props: ['todo'],
+  template: '<li>{{ todo }}</li>'
+})
+
+
+let list = new Vue({
+  el: '#list',
+  data: {
+    items: [{item: 'item1'},{item: 'item2'},{item: 'item3'}]
+  }
+})
