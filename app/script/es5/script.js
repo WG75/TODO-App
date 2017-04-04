327999668ae35d2be1b56d7bd14adf7207898e66
Doubleu Gee <Waleed.gamal1000@gmail.com>
2017-04-04 23:35:25 +0200

    add and remove todos using vanilla javascript

--- app/script/es5/script.js
+++ app/script/es5/script.js
@@ -1,13 +1,39 @@
 'use strict';
 
-Vue.component('hi', {
-  props: ['todo'],
-  template: '<li>{{ todo }}</li>'
-});
+(function () {
+  "use strict";
 
-var list = new Vue({
-  el: '#list',
-  data: {
-    items: [{ item: 'item1' }, { item: 'item2' }, { item: 'item3' }]
-  }
-});
+  var addButton = document.querySelector('.add');
+  var removeButton = document.querySelector('.remove');
+  var input = document.querySelector('.add-todo');
+  var todoTemplate = document.querySelector('.todo-template');
+  var appList = document.querySelector('.app');
+  var app = {};
+
+  app.getInputValue = function getValue() {
+    var val = input.value;
+
+    if (!val) return;
+
+    app.addTodo(val);
+  };
+
+  app.addTodo = function addTodo(val) {
+    var todoClone = todoTemplate.cloneNode(true);
+
+    todoClone.classList.remove('todo-template');
+    todoClone.querySelector('.todo-text').textContent = val;
+
+    appList.appendChild(todoClone);
+  };
+
+  addButton.addEventListener('click', app.getInputValue);
+
+  appList.addEventListener('click', function (e) {
+    var target = e.target;
+
+    if (target.classList.contains('remove')) {
+      target.parentNode.remove();
+    }
+  });
+})();
--- app/script/es6/script.js
+++ app/script/es6/script.js
@@ -1,12 +1,40 @@
-Vue.component('hi', {
-  props: ['todo'],
-  template: '<li>{{ todo }}</li>'
-})
+(() => {
+  "use strict";
+
+  const addButton = document.querySelector('.add');
+  const removeButton = document.querySelector('.remove');
+  const input = document.querySelector('.add-todo');
+  const todoTemplate = document.querySelector('.todo-template')
+  const appList = document.querySelector('.app');
+  const app = {};
 
 
-let list = new Vue({
-  el: '#list',
-  data: {
-    items: [{item: 'item1'},{item: 'item2'},{item: 'item3'}]
+  app.getInputValue = function getValue(){
+    var val = input.value
+
+    if(!val) return;
+
+    app.addTodo(val);
   }
-})
+
+  app.addTodo = function addTodo(val){
+    var todoClone = todoTemplate.cloneNode(true);
+
+    todoClone.classList.remove('todo-template')
+    todoClone.querySelector('.todo-text').textContent = val;
+
+    appList.appendChild(todoClone);
+  }
+
+
+  addButton.addEventListener('click', app.getInputValue);
+
+
+  appList.addEventListener('click', (e) => {
+    var target = e.target;
+
+    if(target.classList.contains('remove')){
+      target.parentNode.remove();
+    }
+  })
+})();
